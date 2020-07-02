22d07e8ca1182dd6ef2c69fdd17c922c900cd40d
kafri bung <kafribung07@gmail.com>
2020-07-02 16:41:49 +0800

    route after login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    View Dekranasda SULSEL
                 </div>
 
                 <div class="links">
